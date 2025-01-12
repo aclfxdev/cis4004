f66dc3b0d040102c43fb49d7089c6a89d1ea859d
Charles <77157467+aclfxdev@users.noreply.github.com>
2025-01-11 21:05:09 -0500

    Update scripts.js
    Added geolocation request code from:
    https://geo-jobe.com/mapthis/tutorial-create-your-own-weather-station-using-web-apis/

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 // Scripts for Web APIs below (later).
+
+if (navigator.geolocation)
+{
+  navigator.geolocation.getCurrentPosition((position) => {console.log(position)}, (error) => console.error("%cError getting location: ${error.message}", "color:red"))
+}
+
+else
+{
+  console.error("%cGeolocation is not supported by this browser!", "color: red")
+}
