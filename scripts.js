da3d30f3e483416d31758d2fe6d21f166356cfb5
Charles <77157467+aclfxdev@users.noreply.github.com>
2025-01-13 19:42:41 -0500

    Update scripts.js, resolve Â symbol
    Hopefully resolve Â symbol issue with char encoding.

--- scripts.js
+++ scripts.js
@@ -77,42 +77,44 @@ async function getForecast(hourlyForecastUrl) {
 }
 
 // Creating the weather cards function
-function createWeatherCards(periods) {
+function createWeatherCards(periods) 
+{
     // Store the card container div from index.html
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = ""; // Clear existing cards if any
-
+    
     // Create a card for each element in the periods array
-    periods.forEach((element) => {
+    periods.forEach((element) => 
+    {
         // Create the html elements
         const div = document.createElement("div");
         const h2 = document.createElement("h2");
         const img = document.createElement("img");
         const p = document.createElement("p");
-
+    
         // Set the icon to be used as the large version
         const icon = element.icon.replace("medium", "large");
-
+    
         // Set the new div class to "card"
         div.setAttribute("class", "card");
-
+    
         // Set the new img attributes
         img.setAttribute("src", icon);
         img.setAttribute("alt", element.shortForecast);
-
+    
         // Set the text content for the card title and forecast
         h2.textContent = `${element.startTime.split("T")[1].slice(0, 5)}: ${element.name}`;
         p.textContent = `${element.shortForecast}, Temp: ${element.temperature}°${element.temperatureUnit}`;
-
+    
         // Append the title, image, and forecast to the card div
         div.appendChild(h2);
         div.appendChild(img);
         div.appendChild(p);
-
+    
         // Append the card to the card container div
         cardContainer.appendChild(div);
     });
-
+    
     // Set favicon to the first weather icon
     const head = document.getElementsByTagName("head")[0];
     const link = document.createElement("link");
@@ -120,3 +122,4 @@ function createWeatherCards(periods) {
     link.href = periods[0].icon.replace("medium", "small");
     head.appendChild(link);
 }
+
