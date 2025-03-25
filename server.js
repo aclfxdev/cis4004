a135967b15727dcfe21f76fb97389428322bceec
Jack Miller <160361153+jackdmiller@users.noreply.github.com>
2025-03-25 14:56:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'cis4004-server.mysql.database.azure.com',
   user: 'borptpseaw@cis4004-server',
-  password: process.env.Admin.cis4004,
+  password: process.env.DB_PASSWORD,
   database: 'weatherdb',
   ssl: { rejectUnauthorized: true }
 });
