0cf7821253897b6e5aa5490a72add5024497c49d
Jack Miller <160361153+jackdmiller@users.noreply.github.com>
2025-04-08 17:15:36 -0400

    Update server.js SSL FIX????

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+      minVersion: 'TLSv1.2'  // ✅ Azure-compatible secure connection
+    }
 });
 
 db.getConnection((err, connection) => {
